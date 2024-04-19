cf19fe63ffd725481c9d2059bee32c4ae43a12fd
DionNuijens <d.nuijens@student.fontys.nl>
2024-04-19 14:21:58 +0200

    Get Summoner

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,9 @@ const API_KEY = "RGAPI-239b5ffb-b5e1-4322-bfe7-382319de9f9a";
 
   function searchForPlayer(event){
     // Set up de correcte api call
-    var APICallingAccountString = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ searchName +"/" + searchTag + "?api_key=" + API_KEY;
+    var APICallingAccountString = `https://localhost:7097/api/Values/Summoner?gameName=${searchName}&tagLine=${searchTag}`;
+    // var APICallingAccountString = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ searchName +"/" + searchTag + "?api_key=" + API_KEY;
+
     //Handel de API Call
     axios.get(APICallingAccountString).then(function (response){
       //Success
@@ -31,12 +33,14 @@ const API_KEY = "RGAPI-239b5ffb-b5e1-4322-bfe7-382319de9f9a";
       <div className='container'>
         <h5>League of Legends Plaayer Searcher</h5>
         <input type='text' onChange={e => setSearchName(e.target.value)}></input>
+        <input type='text' onChange={e => setSearchTag(e.target.value)}></input>
+
         {/* <input type='text' onChange={e => setSearchTag(e.target.value)}></input> */}
         <button onClick={e => searchForPlayer(e)}>Searcher For Player</button>
       </div>
       {JSON.stringify(playerData) != '{}' ? 
       <>
-      <p>{playerData.name}</p>
+      <p>{playerData.summonerName}</p>
       <p>{playerData.summonerLevel}</p>
       <img widht="100" height="100" src={"https://ddragon.leagueoflegends.com/cdn/14.5.1/img/profileicon/" + playerData.profileIconId + ".png"}></img>
 
